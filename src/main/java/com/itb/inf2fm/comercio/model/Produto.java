c736234562080a6f08ee47529c90dacbd71c7224
marcos062910 <marcosviniciusproencadeoliveir@gmail.com>
2023-06-06 12:29:32 -0300

    inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,15 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+	private long id;
+    private String nome;
+    private String descrição;
+    private String codigoBarras;
+    private double preço;
+    
+    // public : Acesso liberado para todas as classes.
+    
+    // private : acesso permitido apenas para os membros
+    //          da propia classe.
+    //          Entende-se por membros (atribuidos e ou método)
+}
