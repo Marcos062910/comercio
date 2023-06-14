4eb101db2dd8c9f3b887e047d36c8b4627ed3aea
marcos062910 <marcosviniciusproencadeoliveir@gmail.com>
2023-06-14 08:18:46 -0300

    Ediçao da lojaController

--- src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2fm/comercio/controller/LojaController.java
@@ -1,18 +1,50 @@
 package com.itb.inf2fm.comercio.controller;
 
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itb.inf2fm.comercio.model.Produto;
+
+  
+
+
 @Controller
-@RequestMapping("//comercio/produtos")
+@RequestMapping("/comercio/produtos")
 public class LojaController {
 	
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
 	public String ListarProdutos() {
-		return "prdutos"; 
 		
+		Produto p1 = new Produto();
+			p1.setId(20l);
+			p1.setNome("Maquina de Lavar Brastemp 15 L");
+			p1.setCodigoBarras("123456789");
+			p1.setDescricao("Produto Linha Branca com painel digital ");
+			p1.setPreco(3215.89);
+			
+			Produto p2 = new Produto();
+			p2.setId(20l);
+			p2.setNome("televisor");
+			p2.setCodigoBarras("12398765");
+			p2.setDescricao("televisor tela plana let sansug ");
+			p2.setPreco(3147.89);
+			
+			// Adicionar os produtos á lista
+			
+						listaDeProdutos.add(p1);
+			
+			return "produtos"; 
+			
+			
 	
 	}
 
--- src/main/java/com/itb/inf2fm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -3,13 +3,49 @@ package com.itb.inf2fm.comercio.model;
 public class Produto {
 	private long id;
     private String nome;
-    private String descrição;
+    private String descricao;
     private String codigoBarras;
-    private double preço;
+    private double preco;
     
     // public : Acesso liberado para todas as classes.
     
     // private : acesso permitido apenas para os membros
     //          da propia classe.
     //          Entende-se por membros (atribuidos e ou método)
+    
+    // M´wtodos Setter e getter atribiur e recuperar dados 
+     public void setId(Long id) {
+    	 this.id = id;
+     }
+     public Long getId() {
+    	 return id;
+     }
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+     
+     
 }
+     
